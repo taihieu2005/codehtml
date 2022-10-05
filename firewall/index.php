7bcf2202549694e25b53ce7d411ae560ca6f9c61
Nguyễn Trọng Thảo <107791662+Nttdddd@users.noreply.github.com>
2022-10-05 16:38:03 +0700

    Update index.php

---
+++ firewall/index.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+require('firewall/core.php');
+?>
+
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+<script>
+
+      document.onkeydown = function(e) {
+              if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+                  alert('Anti F12 - Crt + U Pro');
+              }
+              return false;
+      };
+      </script>    
+  <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="shortcut icon" href="assets/img/icon.png" type="image/x-icon">
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <title>TrongThao - Profile</title>
+  </head>
+  <body>
+      <header class="profile container">
+          <i class="ri-moon-line change-theme" id="theme-button"></i>
+          <div class="profile__container grid">
+              <div class="profile__data">
+                  <div class="profile__border">
+                      <div class="profile__perfil">
+         <img src="assets/img/background.gif" alt="avt" style="width:540px;height:120px;">
+                      </div>
+                  </div>
+                  <h2 class="profile__name">Nguyễn Trọng Thảo</h2>
+                  <h3 class="profile__profession">I Love Everyone 😘</h3>
+                  <ul class="profile__social">
+                      <a href="https://www.facebook.com/thao.nt.shinmedia" target="_blank" class="profile__social-link">
+                          <i class="ri-facebook-circle-fill"></i>
+                      </a>
+                      <a href="https://zalo.me/84566362426" target="_blank" class="profile__social-link">
+                          <i class="ri-chat-1-fill"></i>
+                      </a>
+                      <a href="https://github.com/Nttdddd" target="_blank" class="profile__social-link">
+                          <i class="ri-github-fill"></i>
+                      </a>
+                  </ul>
+              </div>
+              <div class="profile__info grid">
+                  <div class="profile__info-group">
+                      <h3 class="profile__info-number">2007</h3>
+                      <p class="profile__info-description">
+                          Year of Birth<br> 
+                      </p>
+                  </div>
+                  <div class="profile__info-group">
+                      <h3 class="profile__info-number">100M +</h3>
+                      <p class="profile__info-description">
+                        Followers<br>
+                      </p>
+                  </div>
+                  <div class="profile__info-group">
+                      <h3 class="profile__info-number">Bắc Giang</h3>
+                      <p class="profile__info-description">Live In<br> </p>
+                  </div>
+              </div>
+              <div class="profile__buttons">
+                  <a href="" class="button">
+                      Music<i class="ri-music-2-fill"></i>
+                  </a>
+              <div class="profile__buttons">
+                  <a href="assets/html/ttmxh.html" class="button">
+                      Inbox<i class="ri-inbox-fill"></i>
+                  </a>
+                </div>
+              <div class="profile__buttons">
+                  <a href="assets/html/contact.html" class="button">
+                      Contact<i class="ri-contacts-book-2-fill"></i>
+                  </a>
+                  </div>
+              </div>
+          </div>
+      </header>
+      <footer class="footer container">
+          <span class="footer__copy">
+              &#169; <a href=""></a> Code by Trọng Thảo
+          </span>
+      </footer>
+      <script src="assets/js/scrollreveal min.js"></script>
+      <script src="assets/js/main.js"></script>
+          <script src="https://cdn.jsdelivr.net/gh/CDNSFree2/heartclick.js@main/heartclick.js"></script>  
+
+ <audio autoplay>
+
+  <source src="assets/audio/vol2.mp3" type="audio/mpeg">
+
+Your browser does not support the audio element.
+</audio>
+
+  <SCRIPT> alert(" < / > Code By : Nguyễn Trọng Thảo (Anti DDoS) < / > ")</SCRIPT>
+
+
+
+<SCRIPT> alert(" Zalo : 0566362426 - Anti DDoS ")</SCRIPT>  </body>
+</html>
